1f8196b31e13740f604804c9f94639f11586a2b8
tomo1833 <m.tomo0912.tm@gmail.com>
2020-03-14 21:49:03 +0900

    setting change

--- website/settings.py
+++ website/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mrrs.apps.MrrsConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
+# UTCから変更:日本標準時
+# TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'USE_I18N = True
 
 USE_L10N = True
 
@@ -117,4 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
+#静的ファイル配置先のディレクトリへのURLです。http://ドメイン名:ポート名/static/フォルダ/ファイル名でアクセスできるようになります。
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
