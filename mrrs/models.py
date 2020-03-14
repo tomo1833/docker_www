ec5a66b6df42fb319d50fe2cb56ecebd018f3e41
tomo1833 <m.tomo0912.tm@gmail.com>
2020-03-14 22:06:26 +0900

    add urls

--- mrrs/models.py
+++ mrrs/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class room_reservation (models.Model):
+    """
+    room_reservationテーブルのモデル.
+
+    """
+    # id : INTEGER型で、主キー
+    id = models.IntegerField(primary_key=True)
+    # 会議室ID : INTEGER型
+    room_id = models.IntegerField()
+    # 利用者 : 文字列型30桁
+    user = models.CharField(max_length=30)
+    # 開始日時 : DATETIME型
+    start_date_time = models.DateTimeField(default=timezone.now)
+    # 終了日時 : DATETIME型
+    end_date_time = models.DateTimeField(default=timezone.now)
+    # 削除フラグ : INTEGER型
+    del_flg = models.IntegerField()
