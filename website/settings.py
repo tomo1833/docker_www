718f2dc0532a3071cd65112a40c1ad36f329196f
tomo1833 <m.tomo0912.tm@gmail.com>
2020-03-15 12:38:25 +0900

    db追加

--- website/settings.py
+++ website/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = 'staticfiles'
