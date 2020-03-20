a6745b2f131936f40bc31ddc9db38b71488265d4
tomo1833 <m.tomo0912.tm@gmail.com>
2020-03-20 12:28:50 +0900

    room

--- mrrs/views.py
+++ mrrs/views.py
@@ -37,7 +37,7 @@ def mrrs(request):
     # データーモデルからデーターを取得する.
     reserv_data = models.room_info.objects.filter(Q(room_reservation__room_id__isnull=True) | q(room_id__isnull=False))
     
-    print(models.room_info.objects.filter(Q(room_reservation__room_id__isnull=True) | q(room_id__isnull=False).query)
+    print(models.room_info.objects.filter(Q(room_reservation__room_id__isnull=True) | q(room_id__isnull=False)).query)
 
     # フォームオブジェクトを取得する.
     form = forms.reserv_room(request.GET or None)
