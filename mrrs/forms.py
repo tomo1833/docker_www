2f8b02e02bda88a553fbd4b6b78259c0f36f39b4
tomo1833 <m.tomo0912.tm@gmail.com>
2020-03-28 23:15:33 +0900

    room

--- mrrs/forms.py
+++ mrrs/forms.py
@@ -27,19 +27,10 @@ class reserv_room(forms.Form):
         widget=forms.TextInput(attrs={"class": "reserve_type_text"}),
     )
     # 開始日時
-    start_date_time = forms.CharField(
-        label="開始日時",
-        max_length=20,
-        required=True,
-        widget=forms.TextInput(attrs={"class": "reserve_type_text"}),
-    )
+    start_date_time = forms.SplitDateTimeField(label="開始日時", required=True,)
+
     # 終了日時
-    end_date_time = forms.CharField(
-        label="終了日時",
-        max_length=20,
-        required=True,
-        widget=forms.TextInput(attrs={"class": "reserve_type_text"}),
-    )
+    end_date_time = forms.SplitDateTimeField(label="終了日時", required=True,)
 
 
 class reserv_room_info(forms.Form):
