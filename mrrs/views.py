9301f688d1e3f8c9710b827953a9b5a3bb3c2c94
tomo1833 <m.tomo0912.tm@gmail.com>
2020-03-28 22:41:58 +0900

    room

--- mrrs/views.py
+++ mrrs/views.py
@@ -98,8 +98,7 @@ def mrrs(request):
         select_room_data.append((str(room.room_id), room.room_name))
 
     # セレクト
-    form.room_id.choices = select_room_data
-    form.room_id.initial = ["1"]
+    form.fields["room_id"].choices = select_room_data
     print("TEST")
     print(form)
 
