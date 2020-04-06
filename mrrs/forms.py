62d5c7552f3c094b884d86a1cd639baed884482a
tomo1833 <m.tomo0912.tm@gmail.com>
2020-04-06 23:40:19 +0900

    room

--- mrrs/forms.py
+++ mrrs/forms.py
@@ -35,13 +35,14 @@ class reserv_room(forms.Form):
 
 
 def clean(self):
-    all_clean_data = super().clean()
-    start_date_time = all_clean_data.get("start_date_time")
-    end_date_time = all_clean_data.get("end_date_time")
+    def clean(self):
+        all_clean_data = super().clean()
+        start_date_time = all_clean_data["start_date_time"]
+        end_date_time = all_clean_data["end_date_time"]
 
-    if start_date_time >= end_date_time:
+        # if start_date_time >= end_date_time:
         raise forms.ValidationError("終了日時は開始日時より後を洗濯して下さい。")
-    return all_clean_data
+        # return all_clean_data
 
 
 class reserv_room_info(forms.Form):
